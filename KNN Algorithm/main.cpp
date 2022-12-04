95508387cd8a99c6996a6d88f1e121488258f08a
Francisco <frab6562@gmail.com>
2022-12-03 22:55:00 -0800

    Started the project
    Added a file read in fucntion!

--- KNN Algorithm/main.cpp
+++ KNN Algorithm/main.cpp
@@ -1,14 +1,57 @@
-//
-//  main.cpp
-//  KNN Algorithm
-//
-//  Created by Francisco Bustamante on 12/3/22.
-//
-
+//Using Files 67 for Small and 39 for Large
 #include <iostream>
+#include <vector>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+// This function reads in the desired file and then places all the appropiate data into their respective vectors.
+void readFile(string file, vector<double> &Class, vector<vector<double>> &feature){
+    ifstream fin;
+    fin.open(file);
+    if (!fin.is_open()){
+        cout << "Could not open file: " << file << endl;
+        return;
+    }
+    
+    int row = 0;
+    double data;
+    fin >> data;
+    Class.push_back(data);
+    feature.push_back(vector<double>());
+    
+    int counter = 0;
+    while(fin >> data){
+        if(counter < 6){
+            feature[row].push_back(data);
+            ++counter;
+        }
+        else{
+            counter = 0;
+            ++row;
+            Class.push_back(data);
+            feature.push_back(vector<double>());
+        }
+    }
+    fin.close();
+}
+
+int main() {
+    string file;
+    int algorithm;
+    //This Matrix will hold all the features
+    vector<vector<double>> features;
+    //This Vector will hold all the Classifications
+    vector<double> classes;
+    
+    cout << "Welcone to the best Feature Selection Algoithm!" << endl;
+    cout << "Type the name of the file you want to run: " << endl;
+    cin >> file;
+    
+    readFile(file, classes, features);
+    
+    cout << "Type the number of the Algoithm you want to run: \n" << "1) Foward Selection\n" << "2) Backward Elimination" << endl;
+    cin >> algorithm;
+    
     return 0;
 }
