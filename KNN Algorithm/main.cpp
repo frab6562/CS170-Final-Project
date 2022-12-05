ea0f80813be11efea62cc18839e8b2ab130de353
Francisco <frab6562@gmail.com>
2022-12-04 17:07:29 -0800

    Added Search Algorithm

--- KNN Algorithm/main.cpp
+++ KNN Algorithm/main.cpp
@@ -2,41 +2,69 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 using namespace std;
 
 // This function reads in the desired file and then places all the appropiate data into their respective vectors.
-bool readFile(string file, vector<double> &Class, vector<vector<double>> &feature){
+bool readFile(string file, vector<double> &classes, vector<vector<double>> &features){
     ifstream fin;
     fin.open(file);
     if (!fin.is_open()){
-        cout << "ERROR: File does no Exist" << file << endl;
+        cout << "ERROR: File does not Exist: " << file << endl;
         return false;
     }
     
     int row = 0;
     double data;
     fin >> data;
-    Class.push_back(data);
-    feature.push_back(vector<double>());
+    classes.push_back(data);
+    features.push_back(vector<double>());
     
     int counter = 0;
     while(fin >> data){
         if(counter < 6){
-            feature[row].push_back(data);
+            features[row].push_back(data);
             ++counter;
         }
         else{
             counter = 0;
             ++row;
-            Class.push_back(data);
-            feature.push_back(vector<double>());
+            classes.push_back(data);
+            features.push_back(vector<double>());
         }
     }
     fin.close();
     return true;
 }
 
+int Leave_One_Out_Cross_Validation(/*vector<double> classes, vector<vector<double>> features, */vector<int> current_set){
+    srand (time(NULL));
+    return rand() % 100;
+}
 
+void Feature_Search(vector<double> classes, vector<vector<double>> features){
+    vector<int> current_set;
+    int best_accuracy = 0;
+    int feature_to_add = 0;
+    int accuracy = 0;
+    
+    for(int i = 0; i < features[0].size(); ++i){
+        cout << "On level " << i+1 << " of the search tree." << endl;
+        best_accuracy = 0;
+        feature_to_add = 0;
+        for(int j = 0; j < features[0].size(); ++j){
+            
+            cout << "-- Considering adding feature " << j+1 << endl;
+            accuracy = Leave_One_Out_Cross_Validation(current_set);
+            if(accuracy > best_accuracy){
+                best_accuracy = accuracy;
+                feature_to_add = j+1;
+            }
+        }
+        current_set.push_back(feature_to_add);
+        cout << "On level " << i+1 << " I added feature " << feature_to_add << " to current set" << endl;
+    }
+}
 
 int main() {
     string file;
@@ -57,6 +85,7 @@ int main() {
     cout << "The dataset has " << features[0].size() << " features (not including class attrubites), with " << features.size() << " instances" << endl;
     cout << "Beginning Search... " << endl;
     
+    Feature_Search(classes, features);
     
     return 0;
 }
