f99ec2dc37907090ca56b3ff980d4685ccf04869
Francisco <frab6562@gmail.com>
2022-12-05 20:28:15 -0800

    Making the Leave_one_out func

--- KNN Algorithm/main.cpp
+++ KNN Algorithm/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include <cmath>
 using namespace std;
 
 // This function reads in the desired file and then places all the appropiate data into their respective vectors.
@@ -37,44 +38,69 @@ bool readFile(string file, vector<double> &classes, vector<vector<double>> &feat
     return true;
 }
 
-int Leave_One_Out_Cross_Validation(/*vector<double> classes, vector<vector<double>> features, */vector<int> current_set){
-    return rand() % 100;
-}
-
-void Feature_Search(vector<double> classes, vector<vector<double>> features){
-    vector<int> current_set;
-    int best_accuracy = 0;
-    int feature_to_add = 0;
-    int accuracy = 0;
-    bool is_in_set = false;
+void Leave_One_Out_Cross_Validation(vector<double> classes, vector<vector<double>> features, vector<int> current_set){
+    double k = classes.size();
+    double distance = 0;
+    double nearest_neighbor_distance = 10000;
+    double nearest_neighbor_location = 0;
+    int nearest_neighbor_label = 0;
     
-    for(int i = 0; i < features[0].size(); ++i){
-        cout << "On level " << i+1 << " of the search tree." << endl;
-        best_accuracy = 0;
-        feature_to_add = 0;
-        for(int j = 0; j < features[0].size(); ++j){
-            for(int k = 0; k < current_set.size(); ++k){ //This extra for loop checks to see if the feature is already in the set.
-                if( j+1 == current_set.at(k))
-                    is_in_set = true;
-            }
-            if(!is_in_set){
-                cout << "-- Considering adding feature " << j+1 << endl;
-                accuracy = Leave_One_Out_Cross_Validation(current_set);
-                if(accuracy > best_accuracy){
-                    best_accuracy = accuracy;
-                    feature_to_add = j+1;
+    for(int i = 0; i < k; ++i){
+        for(int j = 0; j < k; ++j){
+            if(j != i){
+                //cout << "Ask if " << i+1 << " is nearest neighbor with " << j+1 << endl;
+                for(int x = 0; x < current_set.size(); ++x){
+                    distance = pow(features[i][current_set.at(x)]-features[j][current_set.at(x)],2) + distance;
+                    distance = sqrt(distance);
                 }
+                cout << distance << endl;
+                if(distance < nearest_neighbor_distance){
+                    nearest_neighbor_distance = distance;
+                    nearest_neighbor_location = j;
+                    nearest_neighbor_label = classes.at(j);
+                }
+                distance = 0;
             }
-            is_in_set = false;
         }
-        current_set.push_back(feature_to_add);
-        cout << "On level " << i+1 << " I added feature " << feature_to_add << " to current set" << endl;
+        cout << i+1 << " is nearest neighbor with " << nearest_neighbor_location+1 << endl;
     }
 }
+
+//void Feature_Search(vector<double> classes, vector<vector<double>> features){
+//    vector<int> current_set;
+//    int best_accuracy = 0;
+//    int feature_to_add = 0;
+//    int accuracy = 0;
+//    bool is_in_set = false;
+//
+//    for(int i = 0; i < features[0].size(); ++i){
+//        cout << "On level " << i+1 << " of the search tree." << endl;
+//        best_accuracy = 0;
+//        feature_to_add = 0;
+//        for(int j = 0; j < features[0].size(); ++j){
+//            for(int k = 0; k < current_set.size(); ++k){ //This extra for loop checks to see if the feature is already in the set.
+//                if( j+1 == current_set.at(k))
+//                    is_in_set = true;
+//            }
+//            if(!is_in_set){
+//                cout << "-- Considering adding feature " << j+1 << endl;
+//                accuracy = Leave_One_Out_Cross_Validation(classes,features,current_set);
+//                if(accuracy > best_accuracy){
+//                    best_accuracy = accuracy;
+//                    feature_to_add = j+1;
+//                }
+//            }
+//            is_in_set = false;
+//        }
+//        current_set.push_back(feature_to_add);
+//        cout << "On level " << i+1 << " I added feature " << feature_to_add << " to current set\n" << endl;
+//    }
+//}
 
 int main() {
     string file;
     int algorithm;
+    vector<int> intial{0,1,2};
     vector<vector<double>> features; //This Matrix will hold all the features
     vector<double> classes;  //This Vector will hold all the Classifications
     
@@ -89,9 +115,12 @@ int main() {
     cin >> algorithm;
     
     cout << "The dataset has " << features[0].size() << " features (not including class attrubites), with " << features.size() << " instances" << endl;
-    cout << "Beginning Search... " << endl;
+//    cout << "Running nearest neighbor with all " << features[0].size() << " features, using leave-one-out evaluation, I get an accuracy of " << Leave_One_Out_Cross_Validation(classes,features,intial) << "%" << endl;
+    cout << "Beginning Search... \n" << endl;
     
-    Feature_Search(classes, features);
+    
+    Leave_One_Out_Cross_Validation(classes,features,intial);
+    //Feature_Search(classes, features);
     
     return 0;
 }
